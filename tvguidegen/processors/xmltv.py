c877fd21d9fe2c01aa44d348df00de05c2bb442b
7jTV <kas.iptv@gmail.com>
2016-07-29 16:28:02 +0200

    Fix encoding bug
    Improve verbose mode

--- tvguidegen/processors/xmltv.py
+++ tvguidegen/processors/xmltv.py
@@ -6,25 +6,40 @@ from pytz import utc
 
 class Export(object):
 
-    def __init__(self,channels,m3uChannels=None):
+    def __init__(self,channels,m3uChannels=None,verbose=False):
         self.channels = channels
         self.m3uChannels = m3uChannels
+        self.verbose = verbose
 
-    def run(self,filename="data/xmltv/all.xml",output=False,prettyXml=False):
+    def run(self,filename="data/xmltv/all.xml",outputXml=False,prettyXml=False):
         # create XML
         tv = etree.Element('tv')
         tv.set("generator-info-name", "Kas-IPTV generator")
 
         channels = self.channels
 
-        #for n in self.m3uNames:
-        #    print "m3uName: %s" % (n['name'].encode('UTF-8'))
         for channel in channels:
+
+            if self.verbose:
+                print '**********************************************************************************************'
+                print "Process tv guide for %s" % (channel['name'])
+
             tz = timezone(channel['timezone'])
             ch = etree.Element('channel')
             ch.set('id', channel['slug'])
 
-            display_name_variations = self.display_name_variations(channel,True)
+            # Try exact match
+            for idx,ex in enumerate([c['name'].upper().replace(' ','') == channel['name'].upper().replace(' ','').encode('utf-8') for c in self.m3uChannels]):
+                #print ex,idx
+                if ex:
+                    if self.verbose: print "M3U channel %s name found. METHOD: Exact matching" % (self.m3uChannels[idx]['name'].encode('UTF-8'))
+                    display_name_variations = [self.m3uChannels[idx]['name']]
+            else:
+                display_name_variations = self.get_variations(channel,True)
+
+            if self.verbose and len(display_name_variations) > 1 and self.m3uChannels:
+                print "Not m3u matching for %s" % (channel['name'])
+
             for dnv in display_name_variations:
                 display_name = etree.Element('display-name')
                 display_name.text = dnv
@@ -38,6 +53,7 @@ class Export(object):
             tv.append(ch)
 
             l_prog = len(channel['guide'])
+            if self.verbose: print '%d programmation(s) found.' % (l_prog)
             for idx_prog, prog in enumerate(channel['guide']):
 
                 stop = ''
@@ -62,10 +78,12 @@ class Export(object):
 
                 tv.append(programme)
 
+            if self.verbose: print '**********************************************************************************************'
+
         s = etree.tostring(tv,xml_declaration=True,encoding='UTf-8', doctype="<!DOCTYPE tv SYSTEM \"xmltv.dtd\">",pretty_print=prettyXml)
 
         if filename: self.write(s,filename=filename)
-        if output: print s
+        if outputXml: print s
         return s
 
     def write(self,s,filename='all.xml'):
@@ -74,7 +92,7 @@ class Export(object):
         f.write(s)
         f.close()
 
-    def display_name_variations(self,channel,addOriginalName=False):
+    def get_variations(self,channel,addOriginalName=False):
 
         original_name = channel['name']
         variations = []
@@ -101,8 +119,8 @@ class Export(object):
             # Add space between string and digit character
             bvSpaceDigit = ''
             for idx,s in enumerate(bv):
-                if s.isdigit() and idx > 0 and bv[idx - 1] != ' ':
-                    bvSpaceDigit += ' ' + s
+                if s.isdigit() and idx > 0 and bv[idx - 1] != u' ':
+                    bvSpaceDigit += u' ' + s
                 else:
                     bvSpaceDigit += s
             if len(bvSpaceDigit) != len(bv) : # check if are changes in length
@@ -138,24 +156,26 @@ class Export(object):
 
             variations.append(bv + " HD") # Add HD to string's end
             variations.append(bv + " " + channel['country'].upper()) # Add country upper case to string's end
-            variations.append(bv + " " + channel['country'].lower()) # Add country upper case to string's end
+            variations.append(bv + " " + channel['country'].lower()) # Add country lower case to string's end
 
             if original_name.find("+") > -1 :
                 variations.append(bv.replace("+",""))
                 variations.append(bv.replace(" +",""))
                 variations.append(bv.replace("+"," plus"))
                 variations.append(bv.replace("+"," Plus"))
-
             #variations.append(bv)
 
         if addOriginalName : variations.append(original_name)
-
+        if self.verbose: print '%d variations generated.' % (len(variations))
         for ch in self.m3uChannels:
-            if ch['name'] in variations:
-                print "*********** M3u name FOUND on variations *********** ====> ++++ %s ++++" % (ch['name'].encode('UTF-8'))
+
+            if ch['name'] in [v.encode('utf-8') for v in variations]:
+                if self.verbose: print "M3U channel %s name found. METHOD: Variations matching" % (ch['name'].encode('UTF-8'))
                 return [ch['name']] # Return only name found in m3u
-            elif ch['name'].upper().replace(' ','') in [v.replace(' ','') for v in variations]:
-                print "*********** M3u name FOUND on variations whitout spaces *********** ====> ++++ %s ++++" % (ch['name'].encode('UTF-8'))
+            elif ch['name'].upper().replace(' ','') in [v.replace(' ','').encode('utf-8') for v in variations]:
+                if self.verbose: print "M3U channel %s name found. METHOD: Variations upper without spaces matching" % (ch['name'].encode('UTF-8'))
                 return [ch['name']]
+
+
 
         return variations
